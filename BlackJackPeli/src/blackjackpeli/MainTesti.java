e0326e46178f15848477edf8f46ed2a0d679a16e
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-26 12:20:01 +0300

    main testaus

--- BlackJackPeli/src/blackjackpeli/MainTesti.java
+++ BlackJackPeli/src/blackjackpeli/MainTesti.java
@@ -10,5 +10,21 @@ package blackjackpeli;
  * @author s1601378
  */
 public class MainTesti {
-    
+    public static void main(String[] args) {
+        
+        //kortin arvo 2-10:käden summaan lisätään kortin numeroa vastaava arvo(2 lisätään 2, 3 lisätään 3.)
+        PelaajanKasi pelaajanKasi = new PelaajanKasi();
+        pelaajanKasi.otaKortti(new Kortti(2, "Ruutu"));
+        pelaajanKasi.otaKortti(new Kortti(5, "Risti"));
+        System.out.println(pelaajanKasi.selvitaSumma()); //TOIMII 2 + 5 = 7
+        System.out.println("-----------------------------");
+        PelaajanKasi pelaaja11TO13 = new PelaajanKasi();
+        System.out.println("-----------------------------");
+        
+        
+        
+        pelaaja11TO13.otaKortti(new Kortti(11, "Hertta"));
+        pelaaja11TO13.otaKortti(new Kortti(13, "Pata"));
+        System.out.println(pelaaja11TO13.selvitaSumma()); //TOIMII 11 = 10 ja 13 = 10; 10 + 10 = 20
+    }
 }
